8cee53785c9c7e7d6b776d65874212ad3e8eb7b2
Isaac Jessup <ibjessup@gmail.com>
2023-10-23 13:59:13 -0400

    Error Handling
    Routes exceptions to a Teams Webhook

--- libs/azure/functions/blueprints/esquire/audiences/oneview/orchestrators/updater.py
+++ libs/azure/functions/blueprints/esquire/audiences/oneview/orchestrators/updater.py
@@ -3,7 +3,7 @@
 from azure.durable_functions import DurableOrchestrationContext
 from libs.azure.functions import Blueprint
 from urllib.parse import urlparse
-import os
+import json, os, logging
 
 bp = Blueprint()
 
@@ -55,146 +55,208 @@ def esquire_audiences_oneview_segment_updater(
     - Temporary data and history related to the orchestration are cleaned up at the end.
     """
 
-    # Extract input from the context
-    record = context.get_input()
-    azure = {
-        "conn_str": "ONEVIEW_CONN_STR",
-        "container_name": os.environ["TASK_HUB_NAME"] + "-largemessages",
-    }
-
-    if record:
-        s3 = {
-            "access_key": "REPORTS_AWS_ACCESS_KEY",
-            "secret_key": "REPOSTS_AWS_SECRET_KEY",
-            "region": "REPORTS_AWS_REGION",
-            "bucket": record["Bucket"],
+    try:
+        # Extract input from the context
+        record = context.get_input()
+        azure = {
+            "conn_str": "ONEVIEW_CONN_STR",
+            "container_name": os.environ["TASK_HUB_NAME"] + "-largemessages",
         }
-        # Fetch the audience data from S3 keys
-        s3_keys = yield context.call_activity(
-            "s3_list_objects",
-            {
-                **s3,
-                "prefix": record["Folder"],
-            },
-        )
 
-        # Raise exception if no S3 keys are found
-        if not s3_keys:
-            raise Exception(
-                "No S3 files found in {}/{}".format(record["Bucket"], record["Folder"])
+        if record:
+            s3 = {
+                "access_key": "REPORTS_AWS_ACCESS_KEY",
+                "secret_key": "REPOSTS_AWS_SECRET_KEY",
+                "region": "REPORTS_AWS_REGION",
+                "bucket": record["Bucket"],
+            }
+            # Fetch the audience data from S3 keys
+            s3_keys = yield context.call_activity(
+                "s3_list_objects",
+                {
+                    **s3,
+                    "prefix": record["Folder"],
+                },
             )
 
-        # Retrieve and normalize the first 12 audience objects from s3
-        audiences = yield context.task_all(
-            [
-                context.call_activity(
-                    "esquire_audiences_oneview_fetch_s3_data",
+            # Raise exception if no S3 keys are found
+            if not s3_keys:
+                raise Exception(
+                    "No S3 files found in {}/{}".format(
+                        record["Bucket"], record["Folder"]
+                    )
+                )
+
+            # Retrieve and normalize the first 12 audience objects from s3
+            audiences = yield context.task_all(
+                [
+                    context.call_activity(
+                        "esquire_audiences_oneview_fetch_s3_data",
+                        {
+                            "source": {
+                                **s3,
+                                "key": key,
+                            },
+                            "target": {
+                                **azure,
+                                "prefix": "{}/raw".format(context.instance_id),
+                            },
+                        },
+                    )
+                    for key in s3_keys[:11]
+                ]
+            )
+
+            # Filter blobs containing device information
+            devices_blobs = [a for a in audiences if "devices" in a["columns"]]
+
+            # Filter blobs containing address information
+            addresses_blobs = [a for a in audiences if "street" in a["columns"]]
+            if len(addresses_blobs):
+                # Generate header for on-spot processing
+                header_url = yield context.call_activity(
+                    "datalake_simple_write",
                     {
-                        "source": {
-                            **s3,
-                            "key": key,
-                        },
-                        "target": {
-                            **azure,
-                            "prefix": "{}/raw".format(context.instance_id),
-                        },
+                        **azure,
+                        "blob_name": "{}/raw/{}".format(
+                            context.instance_id, "header.csv"
+                        ),
+                        "content": "street,city,state,zip,zip4",
                     },
                 )
-                for key in s3_keys[:11]
-            ]
-        )
 
-        # Filter blobs containing device information
-        devices_blobs = [a for a in audiences if "devices" in a["columns"]]
-
-        # Filter blobs containing address information
-        addresses_blobs = [a for a in audiences if "street" in a["columns"]]
-        if len(addresses_blobs):
-            # Generate header for on-spot processing
-            header_url = yield context.call_activity(
-                "datalake_simple_write",
-                {
-                    **azure,
-                    "blob_name": "{}/raw/{}".format(context.instance_id, "header.csv"),
-                    "content": "street,city,state,zip,zip4",
-                },
-            )
-
-            # Call sub-orchestrator for on-spot processing
-            onspot_results = yield context.call_sub_orchestrator(
-                "onspot_orchestrator",
-                {
-                    "conn_str": azure["conn_str"],
-                    "container": azure["container_name"],
-                    "outputPath": "{}/devices".format(context.instance_id),
-                    "endpoint": "/save/addresses/all/devices",
-                    "request": {
-                        "hash": False,
-                        "name": record["SegmentID"],
-                        "fileFormat": {
-                            "delimiter": ",",
-                            "quoteEncapsulate": True,
+                # Call sub-orchestrator for on-spot processing
+                onspot_errors = []
+                try:
+                    onspot_results = yield context.call_sub_orchestrator(
+                        "onspot_orchestrator",
+                        {
+                            "conn_str": azure["conn_str"],
+                            "container": azure["container_name"],
+                            "outputPath": "{}/devices".format(context.instance_id),
+                            "endpoint": "/save/addresses/all/devices",
+                            "request": {
+                                "hash": False,
+                                "name": record["SegmentID"],
+                                "fileFormat": {
+                                    "delimiter": ",",
+                                    "quoteEncapsulate": True,
+                                },
+                                "mappings": {
+                                    "street": ["street"],
+                                    "city": ["city"],
+                                    "state": ["state"],
+                                    "zip": ["zip"],
+                                    "zip4": ["zip4"],
+                                },
+                                "matchAcceptanceThreshold": 29.9,
+                                "sources": [
+                                    a["url"].replace("https://", "az://")
+                                    for a in (
+                                        [{"url": header_url, "columns": None}]
+                                        + addresses_blobs
+                                    )
+                                ],
+                            },
                         },
-                        "mappings": {
-                            "street": ["street"],
-                            "city": ["city"],
-                            "state": ["state"],
-                            "zip": ["zip"],
-                            "zip4": ["zip4"],
-                        },
-                        "matchAcceptanceThreshold": 29.9,
-                        "sources": [
-                            a["url"].replace("https://", "az://")
-                            for a in (
-                                [{"url": header_url, "columns": None}] + addresses_blobs
-                            )
+                    )
+                except Exception as e:
+                    onspot_errors += [e.args]
+
+                onspot_errors += [
+                    e["message"]
+                    for e in onspot_results["callbacks"]
+                    if not e["success"]
+                ]
+                if onspot_errors:
+                    raise Exception(*onspot_errors)
+
+                # Add the processed device blobs to the list
+                devices_blobs += [
+                    {"url": j["location"].replace("az://", "https://"), "columns": None}
+                    for j in onspot_results["jobs"]
+                ]
+
+            segment_url = yield context.call_sub_orchestrator(
+                "oneview_orchestrator_segment_uploader",
+                {
+                    "source": {
+                        **azure,
+                        "prefix": context.instance_id,
+                        "blob_names": [
+                            "/".join(urlparse(b["url"]).path.split("/")[2:])
+                            for b in devices_blobs
                         ],
+                    },
+                    "segment_id": record["SegmentID"],
+                    "target": {
+                        "access_key": "ONEVIEW_SEGMENTS_AWS_ACCESS_KEY",
+                        "secret_key": "ONEVIEW_SEGMENTS_AWS_SECRET_KEY",
+                        "bucket": "ONEVIEW_SEGMENTS_S3_BUCKET",
+                        "prefix": os.environ["ONEVIEW_SEGMENTS_S3_PREFIX"],
                     },
                 },
             )
 
-            onspot_errors = [
-                e["message"] for e in onspot_results["callbacks"] if not e["success"]
-            ]
-            if onspot_errors:
-                raise Exception(*onspot_errors)
-
-            # Add the processed device blobs to the list
-            devices_blobs += [
-                {"url": j["location"].replace("az://", "https://"), "columns": None}
-                for j in onspot_results["jobs"]
-            ]
-
-        segment_url = yield context.call_sub_orchestrator(
-            "oneview_orchestrator_segment_uploader",
-            {
-                "source": {
-                    **azure,
-                    "prefix": context.instance_id,
-                    "blob_names": [
-                        "/".join(urlparse(b["url"]).path.split("/")[2:])
-                        for b in devices_blobs
-                    ],
+            # Retain a copy of the segment file
+            yield context.call_activity(
+                "datalake_copy_blob",
+                {
+                    "source": segment_url,
+                    "target": {
+                        **azure,
+                        "blob_name": "segments/{}.csv".format(record["SegmentID"]),
+                    },
                 },
-                "segment_id": record["SegmentID"],
-                "target": {
-                    "access_key": "ONEVIEW_SEGMENTS_AWS_ACCESS_KEY",
-                    "secret_key": "ONEVIEW_SEGMENTS_AWS_SECRET_KEY",
-                    "bucket": "ONEVIEW_SEGMENTS_S3_BUCKET",
-                    "prefix": os.environ["ONEVIEW_SEGMENTS_S3_PREFIX"],
-                },
-            },
-        )
-
-        # Retain a copy of the segment file
-        yield context.call_activity(
-            "datalake_copy_blob",
-            {
-                "source": segment_url,
-                "target": {
-                    **azure,
-                    "blob_name": "segments/{}.csv".format(record["SegmentID"]),
-                },
+            )
+    except Exception as e:
+        yield context.call_http(
+            method="POST",
+            uri=os.environ["EXCEPTIONS_WEBHOOK_ADOPS"],
+            content={
+                "@type": "MessageCard",
+                "@context": "http://schema.org/extensions",
+                "themeColor": "EE2A3D",
+                "summary": "OneView Segment Update Error",
+                "sections": [
+                    {
+                        "activityTitle": "OneView Segment Update Error",
+                        "activitySubtitle": "{}{}".format(
+                            str(e)[0:128], "..." if len(str(e)) > 128 else ""
+                        ),
+                        "facts": [
+                            {"name": "SegmentID", "value": record["SegmentID"]},
+                            {"name": "Folder", "value": record["Folder"]},
+                        ],
+                        "markdown": True,
+                    }
+                ],
+                "potentialAction": [
+                    {
+                        "@type": "OpenUri",
+                        "name": "NocoDB",
+                        "targets": [
+                            {
+                                "os": "default",
+                                "uri": "https://nocodb.aks.esqads.com/dashboard/#/nc/pifplkhdufgnyxl/mxhurkrks4sfqrj?rowId={}".format(
+                                    record["Id"]
+                                ),
+                            }
+                        ],
+                    },
+                    {
+                        "@type": "OpenUri",
+                        "name": "AWS S3",
+                        "targets": [
+                            {
+                                "os": "default",
+                                "uri": "https://s3.console.aws.amazon.com/s3/buckets/adops-team-bucket?region=us-east-2&prefix={}/&showversions=false".format(
+                                    record["Folder"]
+                                ),
+                            }
+                        ],
+                    },
+                ],
             },
         )
 
---
+++ libs/azure/functions/blueprints/microsoft_graph/activities/message_team_channel.py
@@ -0,0 +1,49 @@
+from libs.azure.functions import Blueprint
+import httpx
+
+# Create a Blueprint instance for defining Azure Functions
+bp = Blueprint()
+
+
+@bp.activity_trigger(input_name="ingress")
+def activity_microsoft_graph_message_team_channel(ingress: dict):
+    """
+    Send a message to a Microsoft Teams channel using Microsoft Graph API.
+
+    Parameters
+    ----------
+    ingress : dict
+        Dictionary containing details for the message and destination.
+        Expected keys are:
+        - webhook: str, URL of the Microsoft Team's Channel Webhook.
+        - message: str, The content of the message to be sent.
+
+    Returns
+    -------
+    str
+        Response text from the Microsoft Graph API request.
+
+    Raises
+    ------
+    Exception
+        If there's an error during the token acquisition or in the response from Microsoft Graph API.
+
+    Examples
+    --------
+    Within an orchestrator function:
+
+    >>> def orchestrator_function(context):
+    ...     response = yield context.call_activity(
+    ...         'activity_microsoft_graph_message_team_channel',
+    ...         {
+    ...             'webhook': 'https://***.webhook.office.com/webhookb2/***@***/IncomingWebhook/***/***',
+    ...             'message': 'Hello, Teams!'
+    ...         }
+    ...     )
+    """
+
+    # Send email using Microsoft Graph
+    return httpx.post(
+        url=ingress["webhook"],
+        json={"text": ingress["message"]},
+    ).text

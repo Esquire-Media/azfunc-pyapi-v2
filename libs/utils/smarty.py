dbf3187b5bf1ed5a16caf31411777f3d541a4f7c
Isaac Jessup <ibjessup@gmail.com>
2023-10-26 11:58:12 -0400

    use new env name - optionally

--- libs/utils/smarty.py
+++ libs/utils/smarty.py
@@ -3,25 +3,34 @@ from smartystreets_python_sdk.us_street import Lookup as StreetLookup
 import pandas as pd
 import os
 
+
 def get_items_recursive(obj, dict={}):
     """
-    Iterates through all key-value pairs of an object and returns those pairs as a dictionary. 
+    Iterates through all key-value pairs of an object and returns those pairs as a dictionary.
     If any pair's value is itself an object, recursively iterates through that object also.
     """
     # iterate through the dict items at this level
     for k, val in obj.__dict__.items():
         # if an item has its own dict, iterate through that recursively
-        if hasattr(obj.__dict__[k], '__dict__'):
+        if hasattr(obj.__dict__[k], "__dict__"):
             get_items_recursive(obj.__dict__[k], dict)
         else:
-
             # for the items at this level, add them to the persistent list
-            if not '__' in k:  # ignore a few metadata categories like '__doc__'
+            if not "__" in k:  # ignore a few metadata categories like '__doc__'
                 dict[k] = val
-        
+
     return dict
 
-def bulk_validate(df, address_col, city_col=None, state_col=None, zip_col=None, auth_id=os.environ['ss_id'], auth_token=os.environ['ss_token']) -> pd.DataFrame:
+
+def bulk_validate(
+    df,
+    address_col,
+    city_col=None,
+    state_col=None,
+    zip_col=None,
+    auth_id=os.getenv("SMARTY_APP_ID", None),
+    auth_token=os.getenv("SMARTY_API_TOKEN", None),
+) -> pd.DataFrame:
     """
     Accepts a dataframe containing address data in one or more component columns. Returns a dataframe with all returned Smarty columns.
 
@@ -50,7 +59,7 @@ def bulk_validate(df, address_col, city_col=None, state_col=None, zip_col=None, 
 
     # reset index (because we merge on this later)
     df = df.reset_index(drop=True)
-    
+
     # convert all address data to strings
     for col in address_col, city_col, state_col, zip_col:
         if col != None:
@@ -59,7 +68,11 @@ def bulk_validate(df, address_col, city_col=None, state_col=None, zip_col=None, 
     # authentication
     credentials = StaticCredentials(auth_id, auth_token)
     # launch the street lookup client
-    client = ClientBuilder(credentials).with_licenses(["us-core-custom-enterprise-cloud"]).build_us_street_api_client()
+    client = (
+        ClientBuilder(credentials)
+        .with_licenses(["us-core-custom-enterprise-cloud"])
+        .build_us_street_api_client()
+    )
 
     # initialize the first batch and the list to store results
     batch = Batch()
@@ -72,7 +85,7 @@ def bulk_validate(df, address_col, city_col=None, state_col=None, zip_col=None, 
         # add data for the address field
         lookup.street = row[address_col]
 
-        # check that city field is specified and data is not empty, then set data        
+        # check that city field is specified and data is not empty, then set data
         if city_col != None:
             if len(row[city_col]) > 0:
                 lookup.city = row[city_col]
@@ -86,13 +99,14 @@ def bulk_validate(df, address_col, city_col=None, state_col=None, zip_col=None, 
                 lookup.zipcode = row[zip_col]
 
         # set lookup settings and add to batch
-        lookup.candidates=1 # return only the best candidate
-        lookup.match='invalid' # include best match even if not a valid mailable address
+        lookup.candidates = 1  # return only the best candidate
+        lookup.match = (
+            "invalid"  # include best match even if not a valid mailable address
+        )
         batch.add(lookup)
 
         # send batch once it hits the max batch size (100) or the last row
-        if batch.is_full() or i == len(df)-1:
-           
+        if batch.is_full() or i == len(df) - 1:
             # send the batch
             client.send_batch(batch)
 
@@ -105,18 +119,22 @@ def bulk_validate(df, address_col, city_col=None, state_col=None, zip_col=None, 
                     info_dict = get_items_recursive(best)
                     data_list.append(info_dict.copy())
                 else:
-                    data_list.append({
-                        'dpv_match_code':None,
-                    })
+                    data_list.append(
+                        {
+                            "dpv_match_code": None,
+                        }
+                    )
 
             # restart an empty Batch
             batch = Batch()
-    
+
     # prevent duplicate columns in the output by dropping the original column for any name conflicts
     dupe_cols = [col for col in list(data_list[0].keys()) if col in df.columns]
     original = df.drop(columns=[address_col] + dupe_cols)
 
     # return the cleaned data merged with any non-duplicated columns from the original data
-    cleaned = pd.merge(original, pd.DataFrame(data_list), right_index=True, left_index=True)
+    cleaned = pd.merge(
+        original, pd.DataFrame(data_list), right_index=True, left_index=True
+    )
 
     return cleaned

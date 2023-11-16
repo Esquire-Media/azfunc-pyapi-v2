0065f28df0533a756a03f45331cb73d9878170c7
Isaac Jessup <ibjessup@gmail.com>
2023-11-16 18:14:39 -0500

    validate issuers

--- libs/utils/jwt.py
+++ libs/utils/jwt.py
@@ -1,17 +1,38 @@
 import jwt
 import httpx
-import json
 from jwt.algorithms import RSAAlgorithm
 
-def validate_bearer_token(bearer_token:str, tenant_id:str):
 
-    # find the public JWT keys that will be used to validate the headers
-    jwks_uri = f"https://login.microsoftonline.com/{tenant_id}/discovery/v2.0/keys"
-    response = httpx.get(jwks_uri).json()
-    keys = response['keys']
+def validate_microsoft_token(bearer_token: str, tenant_id: str, client_id: str):
+    token = validate_generic_token(
+        bearer_token=bearer_token,
+        valid_issuers=[
+            f"https://login.microsoftonline.com/{tenant_id}/",
+            f"https://sts.windows.net/{tenant_id}/"
+        ]
+    )
+    if client_id != token["aud"]:
+        raise Exception("Token's audience (app/client id) is not valid.")
+    
+    return token
 
-    # decode the JWT as an unverified header
-    uvh = jwt.get_unverified_header(bearer_token.replace("Bearer ", ""))
+
+def validate_generic_token(bearer_token: str, valid_issuers: list):
+    # sanitize
+    bearer_token = bearer_token.replace("Bearer ", "")
+    # decode the JWT's unverified header without verification
+    uvh = jwt.get_unverified_header(bearer_token)
+    # decode the JWT's payload without verification
+    uvp = jwt.decode(
+        bearer_token,
+        algorithms=[uvh["alg"]],
+        options={"verify_signature": False},
+    )
+    
+    if uvp["iss"] not in valid_issuers:
+        raise Exception("Token's issuer is not valid.")
+
+    keys = httpx.get(f"{uvp['iss']}/discovery/v2.0/keys").json()["keys"]
 
     # find the jwks key which matches the unverified header
     key = next((key for key in keys if key["kid"] == uvh["kid"]), None)
@@ -21,18 +42,11 @@ def validate_bearer_token(bearer_token:str, tenant_id:str):
     # convert to PEM format
     pem_key = RSAAlgorithm.from_jwk(key)
 
-    # unverified decoding to get audience
-    audience = jwt.decode(
-        bearer_token.replace("Bearer ", ""),
-        algorithms=[uvh['alg']],
-        options={'verify_signature':False}
-    )['aud']
-
     # verified decoding with pem_key and audience included
     return jwt.decode(
-        bearer_token.replace("Bearer ", ""),
-        algorithms=[uvh['alg']],
+        bearer_token,
+        algorithms=[uvh["alg"]],
         key=pem_key,
-        audience=audience,
-        options={'verify_signature':False}
+        audience=uvp["aud"],
+        options={"verify_signature": False},
     )

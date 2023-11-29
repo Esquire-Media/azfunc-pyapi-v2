6b80fc564f746a36e87b7f4aa389187b86d784f2
JeamiVW <31642820+JeamiVW@users.noreply.github.com>
2023-11-29 15:30:54 -0500

    Input Changes
    Made this more dynamic by adding 'source' and 'destination' instead of static variables.

--- libs/azure/functions/blueprints/onspot/activities/merge_devices.py
+++ libs/azure/functions/blueprints/onspot/activities/merge_devices.py
@@ -2,6 +2,7 @@
 
 from libs.azure.functions import Blueprint
 from azure.storage.blob import (
+    BlobClient,
     ContainerClient,
     ContainerSasPermissions,
     generate_container_sas,
@@ -15,43 +16,42 @@ bp: Blueprint = Blueprint()
 
 @bp.activity_trigger(input_name="ingress")
 def activity_onSpot_mergeDevices(ingress: dict):
-    # hard coded needs to be replaced with an input we can put into the activity
-    # take 2 params: source(list of uris) and destination(conn string, container name, blob path)
-    
-    # make connection to the container
-    container_client = ContainerClient.from_connection_string(
-        conn_str=os.environ["ONSPOT_CONN_STR"],
-        container_name="general",
+    # source container
+    sources_container_client = ContainerClient.from_connection_string(
+        conn_str=os.environ[ingress["source"]["conn_str"]],
+        container_name=ingress["source"]["container_name"],
     )
     # generate sas token
     sas_token = generate_container_sas(
-        account_name=container_client.account_name,
-        account_key=container_client.credential.account_key,
-        container_name=container_client.container_name,
+        account_name=sources_container_client.account_name,
+        account_key=sources_container_client.credential.account_key,
+        container_name=sources_container_client.container_name,
         permission=ContainerSasPermissions(write=True, read=True),
         expiry=datetime.utcnow() + relativedelta(days=2),
     )
-    destination_blob_client = container_client.get_blob_client(
-        "audiences/" + ingress["audience_id"]
+
+    # make connection to the container
+    destination_blob_client = BlobClient.from_connection_string(
+        conn_str=os.environ[ingress["destination"]["conn_str"]],
+        container_name=ingress["destination"]["container_name"],
+        blob_name=ingress["destination"]["blob_name"],
     )
     first = True
 
-    for blob_name in container_client.list_blob_names(
-        name_starts_with="{}/{}/audiences/{}/devices/".format(
-            ingress["blob_prefix"], ingress["instance_id"], ingress["audience_id"]
-        )
+    for blob_name in sources_container_client.list_blob_names(
+        name_starts_with=ingress["source"]["blob_prefix"]
     ):
         if not blob_name.endswith(".debug.csv"):
             if first:
                 destination_blob_client.create_append_blob()
                 header = (
-                    container_client.download_blob(blob_name, 0, 8).read()
+                    sources_container_client.download_blob(blob_name, 0, 8).read()
                     == b"deviceid"
                 )
                 first = False
 
             destination_blob_client.append_block_from_url(
-                container_client.url + "/" + blob_name + "?" + sas_token,
+                sources_container_client.url + "/" + blob_name + "?" + sas_token,
                 9 if header else None,
             )
 

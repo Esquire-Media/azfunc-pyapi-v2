ed13c87d082d7b788a4675201cb7878b12622346
Isaac Jessup <ibjessup@gmail.com>
2023-11-30 12:31:40 -0500

    Address Validation
    Address validation has been moved to a separate step in the workflow.

--- libs/azure/functions/blueprints/esquire/audiences/maids/addresses/footprint/orchestrator.py
+++ libs/azure/functions/blueprints/esquire/audiences/maids/addresses/footprint/orchestrator.py
@@ -21,7 +21,6 @@ def orchestrator_esquireAudienceMaidsAddresses_footprint(
 ):
     ingress = context.get_input()
     retry = RetryOptions(15000, 1)
-    logging.warning(ingress)
 
     # suborchestrator for the rooftop polys
     poly_batches = yield context.task_all(
@@ -30,33 +29,24 @@ def orchestrator_esquireAudienceMaidsAddresses_footprint(
             context.call_sub_orchestrator_with_retry(
                 "orchestrator_rooftopPolys",
                 retry,
-                valid_chunk.to_list()[:20],
+                chunk.dropna(subset=["delivery_line_1"])
+                .apply(
+                    lambda row: f"{row['delivery_line_1']}, {row['city_name']} {row['state_abbreviation']}, {row['zipcode']}",
+                    axis=1,
+                )
+                .str.upper()
+                .to_list(),
             )
-            for chunk in pd.read_csv(
-                ingress["source"], chunksize=1000, encoding_errors="ignore"
-            )
-            if isinstance((mapped_chunk := detect_column_names(chunk)), pd.DataFrame)
-            if isinstance(
-                (
-                    valid_chunk := bulk_validate(
-                        df=mapped_chunk,
-                        address_col="street"
-                        if "street" in mapped_chunk.columns
-                        else None,
-                        city_col="city" if "city" in mapped_chunk.columns else None,
-                        state_col="state" if "state" in mapped_chunk.columns else None,
-                        zip_col="zipcode"
-                        if "zipcode" in mapped_chunk.columns
-                        else None,
-                    )
-                    .dropna(subset=["delivery_line_1"])
-                    .apply(
-                        lambda row: f"{row['delivery_line_1']}, {row['city_name']} {row['state_abbreviation']}, {row['zipcode']}",
-                        axis=1,
-                    )
-                    .str.upper()
-                ),
-                pd.Series,
+            for chunk in pd.read_parquet(
+                ingress["source"],
+                columns=[
+                    "delivery_line_1",
+                    "city_name",
+                    "state_abbreviation",
+                    "zipcode",
+                ],
+                chunksize=1000,
+                encoding_errors="ignore",
             )
         ]
     )

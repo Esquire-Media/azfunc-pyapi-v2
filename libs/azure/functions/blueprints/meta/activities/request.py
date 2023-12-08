1206773aaac8880a2af42232ba7b15c5bdcf8c34
Isaac Jessup <ibjessup@gmail.com>
2023-12-08 09:39:31 -0500

    refactor
    Changed how handling pagination is determined and how data is parsed.
    If an error is detected, activity will immediate return with the error data.

--- libs/azure/functions/blueprints/meta/activities/request.py
+++ libs/azure/functions/blueprints/meta/activities/request.py
@@ -2,44 +2,105 @@
 
 from libs.azure.functions import Blueprint
 from libs.openapi.clients import Meta
-import json, os
+import json, os, pandas as pd, logging
 
 bp = Blueprint()
 
 
 @bp.activity_trigger(input_name="ingress")
 def meta_activity_request(ingress: dict):
+    """
+    An Azure Durable Function activity to handle API requests.
+
+    This function takes an input dictionary 'ingress' that specifies an API
+    operation, security credentials, and optional parameters for data handling.
+    It performs the API request and processes the response, including storing
+    the results in Azure Blob Storage if specified.
+
+    Parameters
+    ----------
+    ingress : dict
+        A dictionary containing necessary information for the API request. It must
+        include the 'operationId' key to identify the API operation. Optionally, it
+        can contain 'access_token', 'data', 'parameters', 'destination', and 'return'
+        keys for further request customization.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the headers, data, and possibly an error message or
+        the next page token for the API response.
+
+    """
+    # Initialize the Meta API client factory based on the operationId
     factory = Meta[ingress["operationId"]]
+
+    # Set the access token for API security, preferring the ingress token, then environment variable
     factory.security.setdefault(
         "access_token",
         os.environ.get(
-            ingress.get("access_token", ""), 
-            os.environ.get("META_ACCESS_TOKEN", "")
+            ingress.get("access_token", ""), os.environ.get("META_ACCESS_TOKEN", "")
         ),
     )
+
+    # Perform the API request
+    match ingress["operationId"]:
+        case "AdAccount_GetInsights":
+            logging.warning(ingress["parameters"]["AdAccount-id"])
     headers, response, _ = factory.request(
         data=ingress.get("data", None),
         parameters=ingress.get("parameters", None),
     )
 
-    if ingress.get("destination", False) and getattr(response, "data", False):
-        from azure.storage.blob import BlobClient
-        import pandas as pd, uuid
+    # Handle any errors in the response
+    if getattr(response, "error", False):
+        return {"headers": headers, "error": response.error}
 
-        blob = BlobClient.from_connection_string(
-            conn_str=os.environ[
-                ingress["destination"].get("conn_str", "AzureWebJobsStorage")
-            ],
-            container_name=ingress["destination"]["container"],
-            blob_name="{}/{}.parquet".format(
-                ingress["destination"]["path"], uuid.uuid4().hex
-            ),
-        )
-        blob.upload_blob(
-            pd.DataFrame(response.model_dump()["data"]).to_parquet(index=False)
-        )
+    url = None
+    # Process and optionally store the response data
+    if getattr(response, "data", False):
+        data = response.model_dump()["data"]
+        if ingress.get("destination", False):
+            from azure.storage.blob import BlobClient
+            import uuid
 
+            # Create a BlobClient to upload the data to Azure Blob Storage
+            blob = BlobClient.from_connection_string(
+                conn_str=os.environ[
+                    ingress["destination"].get("conn_str", "AzureWebJobsStorage")
+                ],
+                container_name=ingress["destination"]["container_name"],
+                blob_name="{}/{}.parquet".format(
+                    ingress["destination"]["blob_prefix"], uuid.uuid4().hex
+                ),
+            )
+            # Upload the data as a parquet file
+            blob.upload_blob(pd.DataFrame(data).to_parquet(index=False))
+            url = blob.url
+
+    next = None
+    # Handle pagination if applicable
+    if getattr(response, "paging", False):
+        if getattr(response.paging, "next", False):
+            next = response.paging.cursors.after
+
+    # match ingress["operationId"]:
+    #     case "AdAccount_GetInsights":
+    #         logging.warning((raw.url, headers, response))
+    #     case "AdAccount_GetInsightsAsync":
+    #         logging.warning(ingress["parameters"]["AdAccount-id"])
+
+    # Prepare and return the final response
     return {
         "headers": headers,
-        "data": json.loads(response.model_dump_json()),
+        "data": (
+            (
+                json.loads(response.model_dump_json())["data"]
+                if hasattr(response, "data") and response.data
+                else json.loads(response.model_dump_json())
+            )
+            if ingress.get("return", True)
+            else url
+        ),
+        "next": next,
     }
--- libs/azure/functions/blueprints/meta/orchestrators/request.py
+++ libs/azure/functions/blueprints/meta/orchestrators/request.py
@@ -1,10 +1,10 @@
 # File: libs/azure/functions/blueprints/meta/orchestrators/request.py
 
-from aiopenapi3 import ResponseSchemaError
+# from aiopenapi3 import ResponseSchemaError
 from azure.durable_functions import DurableOrchestrationContext, RetryOptions
 from datetime import datetime, timedelta
 from libs.azure.functions import Blueprint
-import json
+import json, logging
 
 bp = Blueprint()
 
@@ -16,13 +16,19 @@ def meta_orchestrator_request(
     retry = RetryOptions(15000, 5)
     ingress = context.get_input()
     after = None
+    page = 0
     data = []
     schema_retry = 0
     while True:
         if schema_retry > 3:
+            context.set_custom_status(
+                f"Too many retries for Operation {ingress['operationId']}."
+            )
             break
         try:
-            context.set_custom_status("")
+            context.set_custom_status(
+                f"Requesting page {page} for Operation {ingress['operationId']}."
+            )
             response = yield context.call_activity_with_retry(
                 "meta_activity_request",
                 retry,
@@ -34,12 +40,13 @@ def meta_orchestrator_request(
                     },
                 },
             )
-        except ResponseSchemaError:
+        except Exception as e:
+            logging.error(e)
             schema_retry += 1
             continue
         if response:
-            if "error" in response["data"].keys():
-                match response["data"]["error"]["code"]:
+            if "error" in response.keys():
+                match response["error"]["code"]:
                     case 4 | 17 | 80004:
                         if throttle := (
                             max(
@@ -56,7 +63,7 @@ def meta_orchestrator_request(
                         ):
                             timer = datetime.utcnow() + timedelta(minutes=throttle)
                             context.set_custom_status(
-                                f"Throttled until {timer.isoformat()}."
+                                f"Waiting to get page {page}. Throttled until {timer.isoformat()}."
                             )
                             yield context.create_timer(timer)
                             continue
@@ -72,35 +79,28 @@ def meta_orchestrator_request(
                         # 368: The action attempted has been deemed abusive or is otherwise disallowed
                         raise Exception(
                             "{} ({}): {}".format(
-                                response["data"]["error"]["message"],
-                                response["data"]["error"]["code"],
-                                response["data"]["error"].get("error_user_msg", ""),
+                                response["error"]["message"],
+                                response["error"]["code"],
+                                response["error"].get("error_user_msg", ""),
                             )
                         )
-
-            if ingress.get("return", True):
-                if "data" in response["data"]:
-                    if response["data"]["data"] != None:
-                        data.append(response["data"]["data"])
+            else:
+                if response["data"]:
+                    if isinstance(response["data"], list):
+                        data += response["data"]
                     else:
                         data.append(response["data"])
+
+                if ingress.get("recursive", False) and response["next"]:
+                    after = response["next"]
+                    page += 1
+                    continue
                 else:
-                    data.append(response["data"])
-
-            if (
-                ingress.get("recursive", False)
-                and "paging" in response["data"].keys()
-                and isinstance(response["data"]["paging"], dict)
-                and "next" in response["data"]["paging"].keys()
-                and response["data"]["paging"]["next"] != None
-            ):
-                after = response["data"]["paging"]["cursors"]["after"]
-                continue
-
+                    context.set_custom_status(f"All requests completed.")
         break
 
-    if ingress.get("return", True):
-        if isinstance(data[0], list):
-            return [d for l in data for d in l]
+    if ingress.get("return", True) or ingress.get("destination", {}):
+        if len(data) == 1:
+            return data[0]
         return data
     return []

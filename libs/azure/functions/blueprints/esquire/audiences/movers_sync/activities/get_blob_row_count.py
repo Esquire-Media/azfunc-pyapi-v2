efe64b66e3cec671bc1e759cdd0be93aad89790b
Isaac Jessup <ibjessup@gmail.com>
2023-12-14 16:59:21 -0500

    docstrings

--- libs/azure/functions/blueprints/esquire/audiences/movers_sync/activities/get_blob_row_count.py
+++ libs/azure/functions/blueprints/esquire/audiences/movers_sync/activities/get_blob_row_count.py
@@ -1,40 +1,56 @@
-import logging
-import os
-import pandas as pd
-from libs.azure.functions import Blueprint
+# File: libs/azure/functions/blueprints/esquire/audiences/mover_sync/activities/get_blob_row_count.py
+
 from azure.data.tables import TableClient
+from libs.azure.functions import Blueprint
 from libs.data import from_bind
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
+import os, pandas as pd
 
-# Create a Blueprint instance for defining Azure Functions
 bp = Blueprint()
 
 
-# Define an activity function
 @bp.activity_trigger(input_name="settings")
 def activity_moversSync_getBlobRowCount(settings: dict):
-    # connect to Azure synapse cluster
+    """
+    Retrieves the row count of a CSV blob in Azure Synapse and updates the count in Azure Table Storage.
+
+    Parameters
+    ----------
+    settings : dict
+        A dictionary containing the settings for the operation, which include:
+        - 'blob_name': A string specifying the name of the blob in Azure Synapse for which the row count is required.
+        - 'rowCounts_table': A dictionary with connection details for Azure Table Storage, containing:
+            - 'conn_str': The name of the environment variable holding the connection string to Azure Table Storage.
+            - 'table_name': The name of the table in Azure Table Storage where the row count will be updated.
+
+    Returns
+    -------
+    dict
+        An empty dictionary, signifying successful completion of the operation.
+
+    Note
+    ----
+    The 'blob_name' is used to determine the PartitionKey and RowKey in Azure Table Storage.
+    The row count is obtained from the specified blob in Azure Synapse.
+    """
+    # Connect to Azure Synapse cluster
     session: Session = from_bind("audiences").connect()
 
-    # load the blob using Synapse and get a row count
+    # Load the blob using Synapse and get a row count
     row_count = pd.DataFrame(
-        session.execute(
-            text(
-                get_counts_query(blob_names=[settings["blob_name"]])
-            )
-        )
+        session.execute(text(get_counts_query(blob_names=[settings["blob_name"]])))
     )["row_count"][0]
 
-    # write row count to table
+    # Write row count to Azure Table Storage
     table_client = TableClient.from_connection_string(
         conn_str=os.environ[settings["rowCounts_table"]["conn_str"]],
         table_name=settings["rowCounts_table"]["table_name"],
     )
     table_client.upsert_entity(
         entity={
-            "PartitionKey": settings["blob_name"].split('/')[-2],
-            "RowKey": settings["blob_name"].split('/')[-1],
+            "PartitionKey": settings["blob_name"].split("/")[-2],
+            "RowKey": settings["blob_name"].split("/")[-1],
             "RowCount": int(row_count),
         }
     )
@@ -44,28 +60,35 @@ def activity_moversSync_getBlobRowCount(settings: dict):
 
 def get_counts_query(blob_names: list) -> str:
     """
-    Build a query to get the total row count of the CSV blob.
-    """
+    Builds a SQL query to count the number of rows in a specified CSV blob.
 
-    # build query
-    blob_names_str = ",".join([f"'{blob_name}'" for blob_name in blob_names])
-    counts_query = f"""
-    SELECT
-        COUNT(*) AS [row_count]
-    FROM OPENROWSET(
-        BULK ({blob_names_str}),
-        DATA_SOURCE = 'sa_esquiremovers',
-        FORMAT = 'CSV',
-        PARSER_VERSION = '2.0',
-        HEADER_ROW = TRUE,
-        FIELDTERMINATOR = ',',
-        ROWTERMINATOR = '0x0a'
-    ) WITH (
-        [add1] VARCHAR(200) COLLATE Latin1_General_100_BIN2_UTF8,
-        [add2] VARCHAR(100) COLLATE Latin1_General_100_BIN2_UTF8,
-        [city] VARCHAR(100) COLLATE Latin1_General_100_BIN2_UTF8,
-        [st] VARCHAR(2) COLLATE Latin1_General_100_BIN2_UTF8,
-        [zip] VARCHAR(5) COLLATE Latin1_General_100_BIN2_UTF8
-    ) AS [data]
+    Parameters
+    ----------
+    blob_names : list
+        A list of blob names for which the row count is to be calculated.
+
+    Returns
+    -------
+    str
+        The SQL query string to count rows in the specified blobs.
     """
-    return counts_query
+    # Build the SQL query string
+    blob_names_str = ",".join([f"'{blob_name}'" for blob_name in blob_names])
+    return f"""
+        SELECT COUNT(*) AS [row_count]
+        FROM OPENROWSET(
+            BULK ({blob_names_str}),
+            DATA_SOURCE = 'sa_esquiremovers',
+            FORMAT = 'CSV',
+            PARSER_VERSION = '2.0',
+            HEADER_ROW = TRUE,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '0x0a'
+        ) WITH (
+            [add1] VARCHAR(200) COLLATE Latin1_General_100_BIN2_UTF8,
+            [add2] VARCHAR(100) COLLATE Latin1_General_100_BIN2_UTF8,
+            [city] VARCHAR(100) COLLATE Latin1_General_100_BIN2_UTF8,
+            [st] VARCHAR(2) COLLATE Latin1_General_100_BIN2_UTF8,
+            [zip] VARCHAR(5) COLLATE Latin1_General_100_BIN2_UTF8
+        ) AS [data]
+    """
--- libs/azure/functions/blueprints/esquire/audiences/movers_sync/orchestrators/orchestrator_validate_addresses.py
+++ libs/azure/functions/blueprints/esquire/audiences/movers_sync/orchestrators/orchestrator_validate_addresses.py
@@ -1,37 +1,53 @@
-from libs.azure.functions import Blueprint
+# File: libs/azure/functions/blueprints/esquire/audiences/mover_sync/orchestrators/orchestrator_validate_addresses.py
+
 from azure.durable_functions import DurableOrchestrationContext, RetryOptions
-import os
-import logging
-import traceback
+from libs.azure.functions import Blueprint
 
 bp = Blueprint()
 
 
 @bp.orchestration_trigger(context_name="context")
 def orchestrator_moversSync_validateAddresses(context: DurableOrchestrationContext):
+    """
+    Orchestrator function for validating addresses in the Movers Sync process.
+
+    This function identifies chunks of data that lack validated addresses, then
+    processes each chunk to validate the addresses and create corresponding blobs.
+    Finally, it returns a list of datasets affected by the new address validations,
+    indicating which datasets need new CETAS tables generated.
+
+    Parameters
+    ----------
+    context : DurableOrchestrationContext
+        The context for the orchestration, containing methods for interacting with
+        the Durable Functions runtime.
+
+    Returns
+    -------
+    list
+        A list of dataset types that have been affected by the new address validations,
+        indicating a need for new CETAS table generation.
+
+    """
     retry = RetryOptions(15000, 1)
 
-    egress = context.get_input()
+    # Get egress settings from the context
+    ingress = context.get_input()
 
-    # call activity to find the chunks that still need address validation
+    # Identify chunks that still need address validation
     missing_data_list = yield context.call_activity_with_retry(
         "activity_moversSync_getMissingChunks",
         retry,
-        {
-            **egress,
-        },
+        ingress,
     )
 
-    # for each chunk of missing address-validated info, validate that chunk and create a blob for it
+    # Validate addresses for each identified chunk
     for chunk_info in missing_data_list:
         yield context.call_activity_with_retry(
             "activity_moversSync_validateAddressChunk",
             retry,
-            {
-                **egress,
-                "chunk":chunk_info
-            }
+            {**ingress, "chunk": chunk_info},
         )
 
-    # return a list of affected datasets that a new CETAS needs to be generated for
-    return list(set([d['blob_type'] for d in missing_data_list]))
+    # Return a list of affected datasets for CETAS generation
+    return list(set([d["blob_type"] for d in missing_data_list]))

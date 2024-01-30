d0edba91513d58ea849035396d41aca5b4181103
Isaac Jessup <ibjessup@gmail.com>
2024-01-30 13:53:01 -0500

    get creative preview endpoint

---
+++ libs/azure/functions/blueprints/meta/endpoints/creative_preview.py
@@ -0,0 +1,18 @@
+# File: libs/azure/functions/blueprints/meta/endpoints/creative_preview.py
+
+from azure.functions import AuthLevel
+from libs.azure.functions import Blueprint
+from libs.azure.functions.http import HttpRequest, HttpResponse
+from libs.openapi.clients.meta import Meta
+
+bp = Blueprint()
+
+
+@bp.route(
+    route="meta_creative_preview",
+    methods=["GET"],
+    auth_level=AuthLevel.FUNCTION,
+)
+async def meta_endpoint_creativePreview(req: HttpRequest):
+    previews = Meta["AdCreative.Get.Previews"](parameters=req.params)
+    return HttpResponse("".join([p["body"] for p in previews.root.data]))

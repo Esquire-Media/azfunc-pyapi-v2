b929691fb1241ae442085248e1f1d2aed190bde7
Ryan Carroll <ryancarroll770@gmail.com>
2024-03-08 10:37:56 -0500

    Helper function for creating blob SAS urls

--- libs/utils/azure_storage.py
+++ libs/utils/azure_storage.py
@@ -1,3 +1,10 @@
+from azure.storage.blob import(
+    BlobClient,
+    BlobSasPermissions,
+    generate_blob_sas
+)
+from datetime import datetime as dt, timedelta
+
 def query_entities_to_list_of_dicts(entities, partition_name:str='PartitionKey', row_name:str='RowKey'):
     """
     Convert the results of an Azure storage table query into a list of dictionaries with keys renamed.
@@ -19,3 +26,20 @@ def query_entities_to_list_of_dicts(entities, partition_name:str='PartitionKey',
         result.append(converted_entity)
 
     return result
+
+def get_blob_sas(blob:BlobClient, expiry:timedelta=timedelta(days=2), prefix:str="https://") -> str:
+    """
+    Given a BlobClient object and an expiry time, return a SAS url for that blob.
+    """
+    return (
+        blob.url
+        + "?"
+        + generate_blob_sas(
+            account_name=blob.account_name,
+            account_key=blob.credential.account_key,
+            container_name=blob.container_name,
+            blob_name=blob.blob_name,
+            permission=BlobSasPermissions(read=True),
+            expiry=dt.utcnow() + timedelta(days=2),
+        )
+    ).replace("https://", prefix)

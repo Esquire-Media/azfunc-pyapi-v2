cc85f7c745711424b0e6061184273f82b3fc0974
Ryan Carroll <ryancarroll770@gmail.com>
2024-03-08 10:37:28 -0500

    add address_from_placekey endpoint

--- libs/azure/functions/blueprints/esquire/audiences/addresses/address_from_placekey/starter.py
+++ libs/azure/functions/blueprints/esquire/audiences/addresses/address_from_placekey/starter.py
@@ -19,15 +19,16 @@ from libs.utils.pydantic.address import Placekey
 import requests
 
 bp = Blueprint()
-
 freshness_window = timedelta(days=365)
 
 @bp.route(route="esquire/addresses/fromPlacekey", methods=["POST"])
 @bp.durable_client_input(client_name="client")
-async def starter_addresses_fromPlacekey(req: HttpRequest, client: DurableOrchestrationClient):
+async def starter_addresses_fromPlacekey(
+    req: HttpRequest, client: DurableOrchestrationClient
+):
     """
     Takes one or more placekeys, and returns data in the caching table associated with those placekeys.
-    NOTE: The cached data is NOT a complete dataset - address info will not be returned if 
+    NOTE: The cached data is NOT a complete dataset - address info will not be returned if
         that address has never been the result of an address-to-placekey call previously.
 
     Input:
@@ -39,16 +40,9 @@ async def starter_addresses_fromPlacekey(req: HttpRequest, client: DurableOrches
     }
 
     Output:
-    [
-        {
-            "placekey":"222@5xc-pdw-7h5",
-            "street":"6013 MERIDIAN DR",
-            "city":"BRYANT",
-            "state":"AR",
-            "zipcode":"72022"
-        },
-        ...
-    ]
+    {
+        "url":"..."
+    }
     """
 
     # load the request payload as a Pydantic object
@@ -56,7 +50,46 @@ async def starter_addresses_fromPlacekey(req: HttpRequest, client: DurableOrches
 
     instance_id = await client.start_new(
         orchestration_function_name="orchestrator_addresses_fromPlacekey",
-        client_input={"placekeys":list(set(payload['placekeys']))} # dedupe before sending to orchestrator
+        client_input={
+            "placekeys": list(set(payload["placekeys"]))
+        },  # dedupe before sending to orchestrator
+    )
+
+    return client.create_check_status_response(req, instance_id)
+
+
+@bp.route(route="esquire/addresses/csv/fromPlacekey", methods=["POST"])
+@bp.durable_client_input(client_name="client")
+async def starter_addressCSV_fromPlacekey(
+    req: HttpRequest, client: DurableOrchestrationClient
+):
+    """
+    Takes one or more placekeys, and returns a CSV of address data from the caching table associated with those placekeys.
+    NOTE: The cached data is NOT a complete dataset - address info will not be returned if
+        that address has never been the result of an address-to-placekey call previously.
+
+    Input:
+    {
+        "placekeys":[
+            "222@5xc-pdw-7h5",
+            ...
+        ]
+    }
+
+    Output:
+    {
+        "url":"https://esquire..."
+    }
+    """
+
+    # load the request payload as a Pydantic object
+    payload = HttpRequest.pydantize_body(req, PlacekeyPayload).model_dump()
+
+    instance_id = await client.start_new(
+        orchestration_function_name="orchestrator_addressCSV_fromPlacekey",
+        client_input={
+            "placekeys": list(set(payload["placekeys"])),
+        },
     )
 
     return client.create_check_status_response(req, instance_id)

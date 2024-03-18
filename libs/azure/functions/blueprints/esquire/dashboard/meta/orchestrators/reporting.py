f7a8899df5e3dbcd422411d4130f0dc320c94181
Isaac Jessup <ibjessup@gmail.com>
2024-03-18 10:53:35 -0400

    deterministic current datetime

--- libs/azure/functions/blueprints/esquire/dashboard/meta/orchestrators/reporting.py
+++ libs/azure/functions/blueprints/esquire/dashboard/meta/orchestrators/reporting.py
@@ -1,10 +1,9 @@
 # File: libs/azure/functions/blueprints/esquire/dashboard/meta/orchestrators/reporting.py
 
 from azure.durable_functions import DurableOrchestrationContext, RetryOptions
-from datetime import datetime, timedelta
+from datetime import timedelta
 from libs.azure.functions import Blueprint
 from libs.azure.functions.blueprints.esquire.dashboard.meta.config import PARAMETERS
-import orjson
 
 bp = Blueprint()
 
@@ -64,11 +63,9 @@ def esquire_dashboard_meta_orchestrator_reporting(
         if not report_run.get("report_run_id", None):
             tries += 1
             context.set_custom_status(f"On try #{tries}")
-            with open("logging.log", "ab") as f:
-                f.write(orjson.dumps(report_run))
             if tries > 3:
                 raise Exception("Insight report generation failed.")
-            yield context.create_timer(datetime.utcnow() + timedelta(minutes=5))
+            yield context.create_timer(context.current_utc_datetime + timedelta(minutes=5))
             continue
         
         
@@ -98,7 +95,7 @@ def esquire_dashboard_meta_orchestrator_reporting(
                     context.set_custom_status(status)
                     break
                 case _:
-                    yield context.create_timer(datetime.utcnow() + timedelta(minutes=1))
+                    yield context.create_timer(context.current_utc_datetime + timedelta(minutes=1))
 
         if status["async_status"] == "Job Completed":
             break
@@ -107,7 +104,7 @@ def esquire_dashboard_meta_orchestrator_reporting(
             context.set_custom_status(f"On try #{tries}")
             if tries > 3:
                 raise Exception("Insight report generation failed.")
-            yield context.create_timer(datetime.utcnow() + timedelta(minutes=1))
+            yield context.create_timer(context.current_utc_datetime + timedelta(minutes=1))
 
     # Download the generated report
     context.set_custom_status(

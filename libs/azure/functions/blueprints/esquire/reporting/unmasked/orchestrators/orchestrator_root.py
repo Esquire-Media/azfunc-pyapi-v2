4802d78bf747d158d5874418495e350ee1926e0b
Ryan Carroll <ryancarroll770@gmail.com>
2024-03-20 12:18:49 -0400

    Add error handling & deployment.py

--- libs/azure/functions/blueprints/esquire/reporting/unmasked/orchestrators/orchestrator_root.py
+++ libs/azure/functions/blueprints/esquire/reporting/unmasked/orchestrators/orchestrator_root.py
@@ -14,33 +14,48 @@ def orchestrator_pixelPush_root(context: DurableOrchestrationContext):
     """
     Coordinate the batching of all Pixel Push tasks across each client.
     """
+    try:
+        ingress = context.get_input()
+        instance_id = context.instance_id
+        retry = RetryOptions(15000,1)
 
-    ingress = context.get_input()
-    instance_id = context.instance_id
-    retry = RetryOptions(15000,1)
-
-    # parse the pixelRoutes table to get a list of pulls needed to run
-    pixel_routes = yield context.call_activity_with_retry(
-        "activity_pixelPush_readRoutesTable",
-        retry,
-        {}
-    )
-
-    # for each pixel data pull, coordinate the required tasks
-    yield context.task_all([
-        context.call_sub_orchestrator_with_retry(
-            "orchestrator_pixelPush_job",
+        # parse the pixelRoutes table to get a list of pulls needed to run
+        pixel_routes = yield context.call_activity_with_retry(
+            "activity_pixelPush_readRoutesTable",
             retry,
+            {}
+        )
+
+        # for each pixel data pull, coordinate the required tasks
+        yield context.task_all([
+            context.call_sub_orchestrator_with_retry(
+                "orchestrator_pixelPush_job",
+                retry,
+                {
+                    "client":route["PartitionKey"],
+                    "data_pull_name":route["RowKey"],
+                    "formatting_orchestrator":route["formatting_orchestrator"],
+                    "webhook_url":route["url"],
+                    "parent_instance": instance_id,
+                    **ingress
+                }
+            ) for route in pixel_routes
+        ])
+
+    except Exception as e:
+        # if any errors are caught, post an error card to teams tagging Isaac
+        yield context.call_activity(
+            "activity_microsoftGraph_postErrorCard",
             {
-                "client":route["PartitionKey"],
-                "data_pull_name":route["RowKey"],
-                "formatting_orchestrator":route["formatting_orchestrator"],
-                "webhook_url":route["url"],
-                "parent_instance": instance_id,
-                **ingress
-            }
-        ) for route in pixel_routes
-    ])
+                "function_name": "esquire-unmasked-push",
+                "instance_id": context.instance_id,
+                "owners":["8489ce7c-e89f-4710-9d34-1442684ce7fe"],
+                "error": f"{type(e).__name__} : {e}"[:1000],
+                "webhook": os.environ["EXCEPTIONS_WEBHOOK_DEVOPS"],
+            },
+        )
+        logging.warning("Error card sent")
+        raise e
 
     # Call sub-orchestrator to purge the instance history
     yield context.call_sub_orchestrator(

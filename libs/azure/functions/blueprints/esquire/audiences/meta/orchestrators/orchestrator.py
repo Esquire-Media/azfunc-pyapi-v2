a9f3b90dceda29944508ff1728b1a94284be74f1
JeamiVW <31642820+JeamiVW@users.noreply.github.com>
2024-05-16 17:12:23 -0400

    Code clean up and implementation of new code

--- libs/azure/functions/blueprints/esquire/audiences/meta/orchestrators/orchestrator.py
+++ libs/azure/functions/blueprints/esquire/audiences/meta/orchestrators/orchestrator.py
@@ -14,106 +14,117 @@ def meta_customaudience_orchestrator(
     context: DurableOrchestrationContext,
 ):
     ingress = context.get_input()
-    # ingress={
-    #     "meta":{
-    #         "adaccountid":"act_947970545616788",
-    #         "audienceid":"120208598420700391",
-    #     },
-    #     "esq":{
-    #         "audienceid":"clulpbfdg001v12jixniohdne", #not a venue replay
-    #     }
-    # },
+    # ingress="clulpbfdg001v12jixniohdne"
 
-    newAudienceNeeded = not ingress["meta"]["audienceid"]
+    # reach out to audience definition DB - get adaccount and audienceid (if it exists)
+    # ids = yield context.call_activity(
+    #     "activity_esquireAudienceMeta_fetchAudience", ingress
+    # )
 
-    if not newAudienceNeeded:
-        metaAudience = yield context.call_sub_orchestrator(
-            "meta_orchestrator_request",
-            {
-                "operationId": "CustomAudience.Get",
-                "parameters": {
-                    "CustomAudience-Id": ingress["meta"]["audienceid"],
-                },
-            },
-        )
-        newAudienceNeeded = bool(metaAudience.get("error", False))
+    # newAudienceNeeded = not metaAudience["audience"]
 
-    # if there is no facebook audience ID
-    if newAudienceNeeded:
-        # create a new audience in facebook
-        context.set_custom_status("Creating new Meta Audience.")
-        metaAudience = yield context.call_sub_orchestrator(
-            "meta_orchestrator_request",
-            {
-                "operationId": "AdAccount.Post.Customaudiences",
-                "parameters": {
-                    "AdAccount-Id": ingress["meta"]["adaccountid"],
-                    "name": "testName",
-                    "description": ingress["esq"]["audienceid"],
-                    "customer_file_source": "USER_PROVIDED_ONLY",
-                    "subtype": "CUSTOM",
-                },
-            },
-        )
+    # if not newAudienceNeeded:
+    #     metaAudience = yield context.call_sub_orchestrator(
+    #         "meta_orchestrator_request",
+    #         {
+    #             "operationId": "CustomAudience.Get",
+    #             "parameters": {
+    #                 "CustomAudience-Id": ids["audience"],
+    #             },
+    #         },
+    #     )
+    #     newAudienceNeeded = bool(metaAudience.get("error", False))
+
+    # # if there is no facebook audience ID
+    # if newAudienceNeeded:
+    #     # create a new audience in facebook
+    #     context.set_custom_status("Creating new Meta Audience.")
+    #     metaAudience = yield context.call_sub_orchestrator(
+    #         "meta_orchestrator_request",
+    #         {
+    #             "operationId": "AdAccount.Post.Customaudiences",
+    #             "parameters": {
+    #                 "AdAccount-Id": ids["adAccount"],
+    #                 "name": "testName",  # this needs to be dynamically generated, static until this is possible.
+    #                 "description": ingress,
+    #                 "customer_file_source": "USER_PROVIDED_ONLY",
+    #                 "subtype": "CUSTOM",
+    #             },
+    #         },
+    #     )
+    #     # need to update the database to have that new audience ID - there is no column in DB for this at this time
 
     # activity to get the number of MAIDs
+    blobs_path = yield context.call_activity(
+        # "activity_esquireAudiencesMeta_getTotalMaids",
+        "activity_esquireAudiencesMeta_newestAudience",
+        {
+            "conn_str": os.environ["ESQUIRE_AUDIENCE_CONN_STR"],
+            "container_name": "general",
+            "audience_id": ingress,
+        },
+    )
+
+    # get list of all blobs in the blob_path
     url_maids = yield context.call_activity(
         "activity_esquireAudiencesMeta_getTotalMaids",
         {
             "conn_str": os.environ["ESQUIRE_AUDIENCE_CONN_STR"],
             "container_name": "general",
-            "blob_name": "audiences/a0H5A00000aZbI1UAK/2023-12-04T18:16:59.757249+00:00/maids.csv",
-            "audience_id": ingress["esq"]["audienceid"],
+            "path_to_blobs": blobs_path,
+            "audience_id": ingress,
         },
     )
-    blob_url, total_maids = url_maids
-    logging.warning(f"{blob_url} {total_maids}")
 
-    # Create the list of tasks 
-    context.set_custom_status("Creating list of user tasks for Meta Audience.")
-    session_id = random.randint(0, 2**32 - 1)
-    task_list = []  # list to hold the tasks
-    batch_seq = 1  # Start the batch sequence at 1
-    last_batch = False
+    logging.warning(url_maids)
 
-    for maids in pd.read_csv(blob_url, header=None, chunksize=1000):
-        maids.columns = ["maids"]
-        task = context.call_sub_orchestrator(
-            "meta_orchestrator_request",
-            {
-                "operationId": "CustomAudience.Post.Usersreplace",
-                "parameters": {
-                    "CustomAudience-Id": ingress["meta"]["audienceid"],
-                    "payload": json.dumps(
-                        {
-                            "schema": "MOBILE_ADVERTISER_ID",
-                            "data_source": {
-                                "type": "THIRD_PARTY_IMPORTED",
-                                "sub_type": "MOBILE_ADVERTISER_IDS",
-                            },
-                            "is_raw": True,
-                            "data": maids["maids"].tolist(),  # list of maids from CSV
-                        }
-                    ),
-                    "session": json.dumps(
-                        {
-                            "session_id": session_id,
-                            "estimated_num_total": total_maids,
-                            "batch_seq": batch_seq,  # Use the current batch sequence
-                            "last_batch_flag": last_batch,  # Assume not the last by default
-                        }
-                    ),
-                },
-            },
-        )
-        task_list.append(task)
-        batch_seq += 1
-        # change the value of last_batch if needed
-        if len(maids) < 1000:
-            last_batch = True
+    return {}
 
-    # run the task_all for the created lsit of tasks to add all the users
-    context.set_custom_status("Adding users to Meta Audience.")
-    metaAudience = yield context.task_all(task_list)
+    # # Create the list of tasks
+    # context.set_custom_status("Creating list of user tasks for Meta Audience.")
+    # session_id = random.randint(0, 2**32 - 1)
+    # task_list = []  # list to hold the tasks
+    # batch_seq = 1  # Start the batch sequence at 1
+    # last_batch = False
 
-    return metaAudience
+    # for maids in pd.read_csv(blob_url, header=None, chunksize=1000):
+    #     maids.columns = ["maids"]
+    #     task = context.call_sub_orchestrator(
+    #         "meta_orchestrator_request",
+    #         {
+    #             "operationId": "CustomAudience.Post.Usersreplace",
+    #             "parameters": {
+    #                 "CustomAudience-Id": ids["audience"],
+    #                 "payload": json.dumps(
+    #                     {
+    #                         "schema": "MOBILE_ADVERTISER_ID",
+    #                         "data_source": {
+    #                             "type": "THIRD_PARTY_IMPORTED",
+    #                             "sub_type": "MOBILE_ADVERTISER_IDS",
+    #                         },
+    #                         "is_raw": True,
+    #                         "data": maids["maids"].tolist(),  # list of maids from CSV
+    #                     }
+    #                 ),
+    #                 "session": json.dumps(
+    #                     {
+    #                         "session_id": session_id,
+    #                         "estimated_num_total": total_maids,
+    #                         "batch_seq": batch_seq,  # Use the current batch sequence
+    #                         "last_batch_flag": last_batch,  # Assume not the last by default
+    #                     }
+    #                 ),
+    #             },
+    #         },
+    #     )
+    #     task_list.append(task)
+    #     batch_seq += 1
+    #     # change the value of last_batch if needed
+    #     if len(maids) < 1000:
+    #         last_batch = True
+
+    # # run the task_all for the created lsit of tasks to add all the users
+    # context.set_custom_status("Adding users to Meta Audience.")
+    # metaAudience = yield context.task_all(task_list)
+
+    # return metaAudience

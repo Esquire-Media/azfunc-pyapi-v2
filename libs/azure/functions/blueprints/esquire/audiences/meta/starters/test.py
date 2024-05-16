2fd5c3b894172cf49ca1d9380ca092e709dc447e
JeamiVW <31642820+JeamiVW@users.noreply.github.com>
2024-05-16 17:12:38 -0400

    Code clean up

--- libs/azure/functions/blueprints/esquire/audiences/meta/starters/test.py
+++ libs/azure/functions/blueprints/esquire/audiences/meta/starters/test.py
@@ -14,35 +14,7 @@ async def starter_esquireAudiencesMeta_test(
     # Start a new instance of the orchestrator function
     instance_id = await client.start_new(
         orchestration_function_name="meta_customaudience_orchestrator",
-        client_input={
-            "meta":{
-                "adaccountid":"act_947970545616788",
-                "audienceid":"120208598420700391",
-                # "audienceid": None
-            },
-            "esq":{
-                "audienceid":"clulpbfdg001v12jixniohdne", #not a venue replay
-            }
-        },
+        client_input="clulpbfdg001v12jixniohdne"
     )
-        
-    #     client_input={
-    #         "source": {
-    #             "conn_str": "ONSPOT_CONN_STR",
-    #             "container_name": "general",
-    #             "blob_prefix": "audiences",
-    #         },
-    #         "working": {
-    #             "conn_str": "ONSPOT_CONN_STR",
-    #             "container_name": "general",
-    #             "blob_prefix": "raw",
-    #         },
-    #         "destination": {
-    #             "conn_str": "ONSPOT_CONN_STR",
-    #             "container_name": "general",
-    #             "blob_prefix": "audiences",
-    #         },
-    #     },
-    # )
-    
+
     return client.create_check_status_response(req, instance_id)

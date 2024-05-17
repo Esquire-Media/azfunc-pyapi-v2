f4cd252e939c6e8acd4dde6363bcad85d0a64d9f
JeamiVW <31642820+JeamiVW@users.noreply.github.com>
2024-05-17 12:55:16 -0400

    Code clean up

--- libs/azure/functions/blueprints/esquire/audiences/meta/starters/test.py
+++ libs/azure/functions/blueprints/esquire/audiences/meta/starters/test.py
@@ -1,5 +1,7 @@
-from libs.azure.functions import Blueprint
+#  file path:libs/azure/functions/blueprints/esquire/audiences/meta/starters/test.py
+
 from azure.durable_functions import DurableOrchestrationClient
+from libs.azure.functions import Blueprint
 from libs.azure.functions.http import HttpRequest
 
 bp = Blueprint()
@@ -14,7 +16,7 @@ async def starter_esquireAudiencesMeta_test(
     # Start a new instance of the orchestrator function
     instance_id = await client.start_new(
         orchestration_function_name="meta_customaudience_orchestrator",
-        client_input="clulpbfdg001v12jixniohdne"
+        client_input="clulpbfdg001v12jixniohdne",
     )
 
     return client.create_check_status_response(req, instance_id)

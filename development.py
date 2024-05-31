5d54256b3561a5fb4574dc6fae848eada956ba68
Isaac Jessup <ibjessup@gmail.com>
2024-05-31 15:53:02 -0400

    dev ready

--- development.py
+++ development.py
@@ -10,7 +10,7 @@ BLUEPRINTS = [
         # "libs/azure/functions/blueprints/keep_alive",
         "libs/azure/functions/blueprints/meta/*",
         # "libs/azure/functions/blueprints/oneview/reports/*",
-        # "libs/azure/functions/blueprints/onspot/*",
+        "libs/azure/functions/blueprints/onspot/*",
         "libs/azure/functions/blueprints/purge_instance_history",
         # "libs/azure/functions/blueprints/smarty/*",
 ]

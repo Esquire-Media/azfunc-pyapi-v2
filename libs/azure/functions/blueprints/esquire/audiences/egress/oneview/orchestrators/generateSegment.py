29eaf9cdc3b5d840036efa4a8e75fbbd2f6268ed
JeamiVW <31642820+JeamiVW@users.noreply.github.com>
2024-06-06 15:24:42 -0400

    Update generateSegment.py
    Updates to the code now takes in the blob information, gets the most recent blobs, makes an append blob and adds all of the needed fields to the CSV file and returns the path to the newly generated file.

--- libs/azure/functions/blueprints/esquire/audiences/egress/oneview/orchestrators/generateSegment.py
+++ libs/azure/functions/blueprints/esquire/audiences/egress/oneview/orchestrators/generateSegment.py
@@ -2,9 +2,14 @@
 
 from azure.durable_functions import DurableOrchestrationContext
 from libs.azure.functions import Blueprint
-from azure.storage.blob import BlobClient
-import pandas as pd
-import logging
+from azure.storage.blob import (
+    BlobClient,
+    BlobSasPermissions,
+    generate_blob_sas,
+)
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import os, pandas as pd
 
 bp = Blueprint()
 
@@ -23,24 +28,50 @@ def orchestrator_esquireAudienceOneView_generateSegment(
     #     "audience_id": audience_id,
     #     "segmentId": ids['segment'],
     # }
-    
-    # Fetch the mpst recent path to the audience files
-    path = yield context.call_activity(
-        "activity_esquireAudiencesUtils_newestAudienceBlobPaths",
-        **ingress['blobInfo'],
-    )
-    
-    logging.warning(path)
-    # 
-    # blob = BlobClient.from_connection_string()
-    # blob
-    
-    
-    # df = pd.read_csv(ingress["blobUrl"])
-    
-    # blob.create_append_blob()
-    # blob.append_block(df.assign(dt="IDFA",si="asdfasdf").to_csv(header=False, index=False))
-    # blob.append_block(df.assign(dt="GOOGLE_AD_ID",si="asdfasdf").to_csv(header=False, index=False))
-    # blob.close()
 
-    return {}
+    # Fetch the mpst recent path to the audience files
+    blobPaths = yield context.call_activity(
+        "activity_esquireAudiencesUtils_newestAudienceBlobPaths",
+        **ingress["blobInfo"],
+    )
+
+    # create the appendblob
+    appendBlob = BlobClient.from_connection_string(
+        conn_str=os.environ[ingress["blobInfo"]["conn_str"]],
+        container_name=ingress["blobInfo"]["container_name"],
+        blob_name=f"{'/'.join(blobPaths[0].split('/')[:-1]) + '/'}appendBlob/blob.csv",
+    )#
+    
+    appendBlob.create_append_blob()
+    # for each blob, read the csv, append them to the new csv
+    for blobPath in blobPaths:
+        blob = BlobClient.from_connection_string(
+            conn_str=os.environ[ingress["blobInfo"]["conn_str"]],
+            container_name=ingress["blobInfo"]["container_name"],
+            blob_name=blobPath,
+        )
+        df = pd.read_csv(
+            blob.url
+            + "?"
+            + generate_blob_sas(
+                account_name=blob.account_name,
+                container_name=blob.container_name,
+                blob_name=blob.blob_name,
+                account_key=blob.credential.account_key,
+                permission=BlobSasPermissions(read=True),
+                expiry=datetime.utcnow() + relativedelta(days=2),
+            )
+        )
+        appendBlob.append_block(
+            df.assign(dt="IDFA", si="asdfasdf").to_csv(header=False, index=False)
+        )
+        appendBlob.append_block(
+            df.assign(dt="GOOGLE_AD_ID", si="asdfasdf").to_csv(
+                header=False, index=False
+            )
+        )
+
+    # close connection to appendBlob
+    appendBlob.close()
+
+    return appendBlob.blob_name

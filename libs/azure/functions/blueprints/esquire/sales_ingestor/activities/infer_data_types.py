32a5b2fdd29fb6f032ab80f68592afe710e29f05
mfair42 <87042201+mfair42@users.noreply.github.com>
2025-07-25 16:37:13 -0400

    logging

--- libs/azure/functions/blueprints/esquire/sales_ingestor/activities/infer_data_types.py
+++ libs/azure/functions/blueprints/esquire/sales_ingestor/activities/infer_data_types.py
@@ -28,7 +28,7 @@ def activity_salesIngestor_inferDataTypes(settings: dict):
         
         date_types = {"DATE", "TIMESTAMP", "DATETIME"}
         if not any(dtype.upper() in date_types for dtype in inferred_types.values()):
-            logger.info(msg=f"[LOG] No date fields were able to be inferred.")
+            logger.error(msg=f"[LOG] No date fields were able to be inferred.")
 
             raise TypeError("No date fields were able to be inferred.")
 

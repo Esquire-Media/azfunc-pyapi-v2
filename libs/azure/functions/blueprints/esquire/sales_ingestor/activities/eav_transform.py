de4daa297109152b27bfa6a49bf2ef824814536c
mfair42 <87042201+mfair42@users.noreply.github.com>
2025-08-15 11:32:34 -0400

    timestamp handling update

--- libs/azure/functions/blueprints/esquire/sales_ingestor/activities/eav_transform.py
+++ libs/azure/functions/blueprints/esquire/sales_ingestor/activities/eav_transform.py
@@ -172,6 +172,7 @@ def activity_salesIngestor_eavTransform(settings: dict):
                 WHEN data_type = 'boolean' THEN 'boolean'
                 WHEN data_type = 'timestamp with time zone' THEN 'timestamptz'
                 WHEN data_type = 'timestamp without time zone' THEN 'timestamptz'
+                WHEN data_type = 'timestamp' THEN 'timestamptz'
                 WHEN data_type IN ('json', 'jsonb') THEN 'jsonb'
                 ELSE 'string'
             END::attr_data_type,
@@ -190,6 +191,7 @@ def activity_salesIngestor_eavTransform(settings: dict):
                 WHEN data_type = 'boolean' THEN 'boolean'
                 WHEN data_type = 'timestamp with time zone' THEN 'timestamptz'
                 WHEN data_type = 'timestamp without time zone' THEN 'timestamptz'
+                WHEN data_type = 'timestamp' THEN 'timestamptz'
                 WHEN data_type IN ('json', 'jsonb') THEN 'jsonb'
                 ELSE 'string'
             END::attr_data_type,

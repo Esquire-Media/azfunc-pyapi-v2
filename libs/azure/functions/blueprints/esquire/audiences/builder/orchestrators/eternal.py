1379d5da87341cb8c555f526dc5ac0457af814ef
mfair42 <87042201+mfair42@users.noreply.github.com>
2025-11-05 15:09:51 -0500

    facebook error handling

--- libs/azure/functions/blueprints/esquire/audiences/builder/orchestrators/eternal.py
+++ libs/azure/functions/blueprints/esquire/audiences/builder/orchestrators/eternal.py
@@ -398,10 +398,12 @@ def orchestrator_esquire_audience(context: DurableOrchestrationContext):
                 }
             )
 
-            yield context.call_sub_orchestrator(
+            res = yield context.call_sub_orchestrator(
                 "orchestrator_esquireAudiences_uploader",
                 build,
             )
+            if type(res) == dict:
+                raise RuntimeError(res['error'])
 
             # Summarize the newly produced run (from storage)
             post_prefix = yield context.call_activity(

b3b08df026c4635cfc379e7d02457ff2eaff3834
mfair42 <87042201+mfair42@users.noreply.github.com>
2025-12-19 12:52:22 -0500

    updated error message

--- libs/azure/functions/blueprints/esquire/reporting/location_insights/activities/get_location_info.py
+++ libs/azure/functions/blueprints/esquire/reporting/location_insights/activities/get_location_info.py
@@ -57,7 +57,7 @@ def activity_locationInsights_getLocationInfo(settings: dict):
     # return the cleaned addresses as a list of component dictionaries, each with an index attribute
     if len(locations) == 0:
         raise Exception(
-            f"Location with ESQ_ID '{settings['locationID']}' was not found in dbo.Locations"
+            f"Location with ESQ_ID '{settings['locationID']}' was not found in keystone.TargetingGeoFrame"
         )
     blob_client: BlobClient = BlobClient.from_connection_string(
         conn_str=os.environ[settings["runtime_container"]["conn_str"]],
